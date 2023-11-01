9c86e24344c115aee60f19af5a3d30d6a28631a0
VDXD <rajputvedant213@gmail.com>
2023-11-01 20:40:20 +0530

    Authentication (still under process)

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("heeloo")
+// var computer = Math.floor(Math.random() * 100 + 1)
+
+// var guess = false
+
+// while(!guess){
+//     var human = prompt("Guess between 1 to 100: ")
+
+//     if(human==computer){
+//         console.log("Correct smart nigga, You guessed: " + human)
+//         guess = true
+//     }
+//     else if (human > computer){
+//         console.log("Go lower nigga, Your guess was: " + human)
+//     }
+//     else {
+//         console.log("Higher nigga, Your guess was: " + human)
+//     }
+// }
+
+
+document.getElementById("register").addEventListener("click", function() {
+    var email =  document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    //For new registration
+    console.log("hello")
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      console.log(user);
+      alert("Registration successfully!!");
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+      console.log(errorMessage);
+      alert(error);
+    });		  		  
+});
